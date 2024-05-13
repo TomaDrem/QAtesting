35ca7f2877c51e3a01a43ea0d2ac070fe9c85768
Toma Drem <tvdrem4@gmail.com>
2024-05-13 22:02:34 +0300

    testing-pytesting

--- tests/api/test_api.py
+++ tests/api/test_api.py
@@ -3,3 +3,19 @@ def test_check_math():
 
 def test_check_78():
     assert 7*8 == 56
+
+class User:
+    def __init__(self) -> None:
+        self.name = 'Viktor'
+        self.second_name = 'Sokol'
+
+user = User()
+def text_remove_name():
+    user.name = ''
+    assert user.name ==''
+def text_name():
+    assert user_name == 'Viktor'
+
+def test_second_name():
+    assert user.second_name() == 'Sokol'
+    
