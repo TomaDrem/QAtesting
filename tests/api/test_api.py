ea57224eb27751759ab4ad092234c4375a9a0521
Toma Drem <tvdrem4@gmail.com>
2024-05-13 22:45:13 +0300

    testing all right

--- tests/api/test_api.py
+++ tests/api/test_api.py
@@ -16,6 +16,28 @@ def text_remove_name():
 def text_name():
     assert user_name == 'Viktor'
 
+
 def test_second_name():
     assert user.second_name() == 'Sokol'
     
+
+import pytest
+
+class User:
+    def __init__(self) -> None:
+        self.name = 'Viktor'
+        self. second_name = 'Sokol'
+
+@pytest.fixture
+def user():
+    yield User()
+
+def test_remove_name(user):
+    user.name = ''
+    assert user.name == ''
+
+def test_name(user):
+    assert user.name == 'Viktor'
+
+def test_second_name(user):
+    assert user.second_name == 'Sokol'
